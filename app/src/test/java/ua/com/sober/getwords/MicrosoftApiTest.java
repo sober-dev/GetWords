986c585cc5f78e3d3a79f844220c7c7f42e2b71b
Dmitry Hmel <dmitry.hmel@gmail.com>
2016-10-14 01:09:47 +0300

    Add MicrosoftApiTest

--- app/src/test/java/ua/com/sober/getwords/MicrosoftApiTest.java
+++ app/src/test/java/ua/com/sober/getwords/MicrosoftApiTest.java
@@ -1,84 +1,77 @@
-package ua.com.sober.getwords.ui.activities;
+package ua.com.sober.getwords;
 
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-
-import com.hannesdorfmann.mosby.mvp.MvpActivity;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import ua.com.sober.getwords.R;
 import ua.com.sober.getwords.mvp.models.MicrosoftTranslatorApiService;
 import ua.com.sober.getwords.mvp.models.mstranslator.ArrayOfGetTranslationsResponse;
 import ua.com.sober.getwords.mvp.models.mstranslator.GetTranslationsArrayRequest;
-import ua.com.sober.getwords.mvp.presenters.MainPresenter;
-import ua.com.sober.getwords.mvp.views.MainView;
 
-public class MainActivity extends MvpActivity<MainView, MainPresenter> implements MainView {
+/**
+ * Created by Dmitry on 14.10.2016.
+ */
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+public class MicrosoftApiTest {
+    private final CountDownLatch latch = new CountDownLatch(1);
+    private MicrosoftTranslatorApiService service;
+    private GetTranslationsArrayRequest request;
+    private ArrayOfGetTranslationsResponse result;
 
-        final Button button = (Button) findViewById(R.id.testMSApi_btn);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                testMSApi();
-            }
-        });
-    }
-
-    @Override
-    public MainPresenter createPresenter() {
-        return new MainPresenter();
-    }
-
-    private void testMSApi() {
+    @Before
+    public void beforeTest() {
+        service = MicrosoftTranslatorApiService.Factory.create();
         GetTranslationsArrayRequest.Texts texts = new GetTranslationsArrayRequest.Texts();
-        List<GetTranslationsArrayRequest.StringWithNamespace> textsList;
-        textsList = Arrays.asList(
+        List<GetTranslationsArrayRequest.StringWithNamespace> textsList = Arrays.asList(
                 new GetTranslationsArrayRequest.StringWithNamespace("lazy"),
                 new GetTranslationsArrayRequest.StringWithNamespace("world"),
                 new GetTranslationsArrayRequest.StringWithNamespace("hole"));
         texts.setTexts(textsList);
         GetTranslationsArrayRequest.Options options = new GetTranslationsArrayRequest.Options();
         options.setState(777);
-
-        GetTranslationsArrayRequest request = new GetTranslationsArrayRequest();
+        request = new GetTranslationsArrayRequest();
         request.setAppId("");
         request.setTexts(texts);
         request.setFrom("en");
         request.setTo("ru");
         request.setMaxTranslations(5);
         request.setOptions(options);
+    }
 
-        MicrosoftTranslatorApiService service = MicrosoftTranslatorApiService.Factory.create();
+    @Test
+    public void testMSApi() throws InterruptedException {
+        Assert.assertNotNull(service);
         Call<ArrayOfGetTranslationsResponse> call = service.getTranslationsArray(request);
-
         call.enqueue(new Callback<ArrayOfGetTranslationsResponse>() {
             @Override
             public void onResponse(Call<ArrayOfGetTranslationsResponse> call, Response<ArrayOfGetTranslationsResponse> response) {
                 if (response.isSuccessful()) {
-                    ArrayOfGetTranslationsResponse result = response.body();
-                    Log.d("Result", result.toString());
+                    result = response.body();
+                    latch.countDown();
                 } else {
-                    Log.d("Error", "Error translations response");
+                    latch.countDown();
                 }
             }
 
             @Override
             public void onFailure(Call<ArrayOfGetTranslationsResponse> call, Throwable t) {
-                Log.d("Error", t.getMessage());
+                latch.countDown();
             }
         });
+        latch.await();
+        Assert.assertNotNull(service);
     }
 
+    @After
+    public void afterTest() {
+        result.toString();
+    }
 }

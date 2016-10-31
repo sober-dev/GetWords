0489048d06642e5ae9d9cf4fc1fb37ccd53ddc7a
Dmitry Hmel <dmitry.hmel@gmail.com>
2016-10-31 23:04:22 +0200

    add Job Manager

---
+++ app/src/main/java/ua/com/sober/getwords/jobs/Priority.java
@@ -0,0 +1,11 @@
+package ua.com.sober.getwords.jobs;
+
+/**
+ * Created by Dmitry on 10/31/2016.
+ */
+
+public class Priority {
+    public static int LOW = 0;
+    public static int MID = 500;
+    public static int HIGH = 1000;
+}
---
+++ app/src/main/java/ua/com/sober/getwords/jobs/SaveResultToDBJob.java
@@ -0,0 +1,39 @@
+package ua.com.sober.getwords.jobs;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.birbit.android.jobqueue.Job;
+import com.birbit.android.jobqueue.Params;
+import com.birbit.android.jobqueue.RetryConstraint;
+
+/**
+ * Created by Dmitry on 10/31/2016.
+ */
+
+public class SaveResultToDBJob extends Job {
+
+    protected SaveResultToDBJob(Params params) {
+        super(params);
+    }
+
+    @Override
+    public void onAdded() {
+
+    }
+
+    @Override
+    public void onRun() throws Throwable {
+
+    }
+
+    @Override
+    protected RetryConstraint shouldReRunOnThrowable(@NonNull Throwable throwable, int runCount, int maxRunCount) {
+        return null;
+    }
+
+    @Override
+    protected void onCancel(int cancelReason, @Nullable Throwable throwable) {
+
+    }
+}
